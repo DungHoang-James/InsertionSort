e8218000cc6646590c2807922d15c08cfb39dc96
DungHoang-James <anhdungit.ni@gmail.com>
2022-09-09 10:30:55 +0700

    init

---
+++ InsertionSort/Program.cs
@@ -0,0 +1,51 @@
+﻿
+using System.Security;
+
+int[] array = { 2, 6, 1, 9, 7, 3, 8, 4, 0, 5 };
+
+Console.WriteLine("Before Sort");
+Console.Write("[");
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i]);
+    if (i < array.Length - 1)
+    {
+        Console.Write("\t");
+    }
+}
+Console.Write("]");
+Console.WriteLine();
+
+// Insertion Sort
+for (int i = 1; i < array.Length; i++)
+{
+    int current = array[i];
+    int position = i;
+
+    for (int j = i - 1; j >= 0; j--)
+    {
+        if (array[j] > current)
+        {
+            array[position] = array[j];
+            position--;
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    array[position] = current;
+}
+
+Console.WriteLine("After Sort");
+Console.Write("[");
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i]);
+    if (i < array.Length - 1)
+    {
+        Console.Write("\t");
+    }
+}
+Console.Write("]");
